c099e9938498de56df728307b99d223aa7efe68c
Wiput Pootong <wiput.pootong@gmail.com>
2018-06-30 22:40:12 +0700

    :tada: initial commit

---
+++ src/common/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <div className="text-center">Footer Text</div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
