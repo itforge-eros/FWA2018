f01776329dd49e4b6012a84628a2286ee35a5b24
Wiput Pootong <wiput.pootong@gmail.com>
2018-07-28 03:50:55 +0700

    :sparkles: add new manifest

--- src/common/components/PageNotFound.js
+++ src/common/components/PageNotFound.js
@@ -3,6 +3,7 @@ import { compose } from 'recompose';
 import { withRouter } from 'react-router-dom';
 
 const enhance = compose(withRouter);
+
 const PageNotFound = () => {
   return <h2 className="text-center">Page not found!</h2>;
 };
